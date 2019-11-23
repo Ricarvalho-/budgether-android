8745b35d382ed91a1125f53f93418180cc4ab15d
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2019-11-23 01:28:07 -0300

    Persistence module creation

---
+++ persistence/src/test/java/br/edu/ifsp/scl/persistence/ExampleUnitTest.kt
@@ -0,0 +1,24 @@
+package br.edu.ifsp.scl.persistence
+
+import android.content.Context
+import androidx.room.Room
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.After
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import java.io.IOException
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
