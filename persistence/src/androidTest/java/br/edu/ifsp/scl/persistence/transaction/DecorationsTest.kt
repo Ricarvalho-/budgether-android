f04189e594de9ab526e31559662cabe7aefe9327
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2019-11-25 14:28:21 -0300

    Transaction repetition before date implementation

---
+++ persistence/src/androidTest/java/br/edu/ifsp/scl/persistence/transaction/DecorationsTest.kt
@@ -0,0 +1,158 @@
+package br.edu.ifsp.scl.persistence.transaction
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import br.edu.ifsp.scl.persistence.observedValue
+import br.edu.ifsp.scl.persistence.shouldBeEqualTo
+import org.junit.Rule
+import org.junit.Test
+import java.util.*
+
+class DecorationsTest {
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    private fun sampleData(
+        title: String = "Sample",
+        category: String = "Sample",
+        value: Double = 0.0,
+        startDate: Date = date(1, 1, 2020),
+        frequency: Transaction.Frequency = Transaction.Frequency.Single,
+        repeat: Int = 0,
+        accountId: Long = 0
+    ) = Transaction.Data(title, category, value, startDate, frequency, repeat, accountId)
+
+    private fun date(day: Int, month: Int, year: Int) = Calendar.getInstance().run {
+        clear()
+        set(year, month.dec(), day)
+        time
+    }
+
+    private fun <T> liveDataListWith(vararg values: T) =
+        MutableLiveData<List<T>>().apply {
+            value = values.asList()
+        } as LiveData<List<T>>
+
+    @Test
+    fun beforeShouldYieldOnlyResultsBeforeSpecifiedDate() {
+        val credit = Transaction.Credit(sampleData(
+            frequency = Transaction.Frequency.Weekly,
+            repeat = 3
+        ))
+
+        val transactions = liveDataListWith(credit)
+        transactions.before(
+            date(14, 1, 2020)
+        ).observedValue?.shouldBeEqualTo(listOf(
+            RepeatingTransaction(credit, date(1, 1, 2020), 1),
+            RepeatingTransaction(credit, date(8, 1, 2020), 2)
+        ))
+    }
+
+    @Test
+    fun beforeShouldYieldInclusiveResults() {
+        val credit = Transaction.Credit(sampleData(
+            frequency = Transaction.Frequency.Weekly,
+            repeat = 3
+        ))
+
+        val transactions = liveDataListWith(credit)
+        transactions.before(
+            date(15, 1, 2020)
+        ).observedValue?.shouldBeEqualTo(listOf(
+            RepeatingTransaction(credit, date(1, 1, 2020), 1),
+            RepeatingTransaction(credit, date(8, 1, 2020), 2),
+            RepeatingTransaction(credit, date(15, 1, 2020), 3)
+        ))
+    }
+
+    @Test
+    fun beforeShouldRespectRepeatability() {
+        val credit = Transaction.Credit(sampleData(
+            frequency = Transaction.Frequency.Weekly,
+            repeat = 3
+        ))
+
+        val transactions = liveDataListWith(credit)
+        transactions.before(
+            date(1, 1, 2021)
+        ).observedValue?.size?.shouldBeEqualTo(3)
+    }
+
+    @Test
+    fun beforeShouldRespectFrequency() {
+        val credit = Transaction.Credit(sampleData(
+            frequency = Transaction.Frequency.Yearly,
+            repeat = 2
+        ))
+
+        val transactions = liveDataListWith(credit)
+        transactions.before(
+            date(1, 1, 2023)
+        ).observedValue?.shouldBeEqualTo(listOf(
+            RepeatingTransaction(credit, date(1, 1, 2020), 1),
+            RepeatingTransaction(credit, date(1, 1, 2021), 2)
+        ))
+    }
+
+    @Test
+    fun beforeShouldYieldResultsOfMultipleTransactions() {
+        val credit = Transaction.Credit(sampleData())
+        val debit = Transaction.Debit(sampleData())
+        val sampleDate = sampleData().startDate
+
+        val transactions = liveDataListWith(credit, debit)
+        transactions.before(
+            sampleDate
+        ).observedValue?.shouldBeEqualTo(listOf(
+            RepeatingTransaction(credit, sampleDate, 1),
+            RepeatingTransaction(debit, sampleDate, 1)
+        ))
+    }
+
+    @Test
+    fun beforeShouldYieldOrderedResults() {
+        val credit = Transaction.Credit(sampleData(
+            startDate = date(2, 2, 2020),
+            frequency = Transaction.Frequency.Monthly,
+            repeat = 2
+        ))
+
+        val debit = Transaction.Debit(sampleData(
+            frequency = Transaction.Frequency.Monthly,
+            repeat = 3
+        ))
+
+        val transactions = liveDataListWith(credit, debit)
+        transactions.before(
+            date(1, 1, 2021)
+        ).observedValue?.shouldBeEqualTo(listOf(
+            RepeatingTransaction(debit, date(1, 1, 2020), 1),
+            RepeatingTransaction(debit, date(1, 2, 2020), 2),
+            RepeatingTransaction(credit, date(2, 2, 2020), 1),
+            RepeatingTransaction(debit, date(1, 3, 2020), 3),
+            RepeatingTransaction(credit, date(2, 3, 2020), 2)
+        ))
+    }
+
+    @Test
+    fun beforeShouldYieldMostReasonableResultsForNonexistentDates() {
+        val credit = Transaction.Credit(sampleData(
+            startDate = date(30, 11, 2019),
+            frequency = Transaction.Frequency.Monthly,
+            repeat = 5
+        ))
+
+        val transactions = liveDataListWith(credit)
+        transactions.before(
+            date(1, 1, 2021)
+        ).observedValue?.shouldBeEqualTo(listOf(
+            RepeatingTransaction(credit, date(30, 11, 2019), 1),
+            RepeatingTransaction(credit, date(30, 12, 2019), 2),
+            RepeatingTransaction(credit, date(30, 1, 2020), 3),
+            RepeatingTransaction(credit, date(29, 2, 2020), 4),
+            RepeatingTransaction(credit, date(30, 3, 2020), 5)
+        ))
+    }
+}
--- persistence/src/main/java/br/edu/ifsp/scl/persistence/transaction/Decorations.kt
+++ persistence/src/main/java/br/edu/ifsp/scl/persistence/transaction/Decorations.kt
@@ -1,7 +1,56 @@
 package br.edu.ifsp.scl.persistence.transaction
 
+import android.util.Range
+import androidx.core.util.rangeTo
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import br.edu.ifsp.scl.persistence.transaction.Transaction.Frequency.*
 import java.util.*
+
+infix fun <T : Transaction> LiveData<List<T>>.before(date: Date) = Transformations.map(this) { items ->
+    items.flatMap { it.repeatWhileAffecting(it.startDate.coerceAtMost(date) rangeTo date) }
+        .sortedBy { it.atDate }
+} as LiveData<List<RepeatingTransaction>>
+
+private infix fun Transaction.repeatWhileAffecting(dateRange: Range<Date>) = sequence {
+    var currentDate = startDate
+    var currentRepetition = 1
+
+    fun notExceededRangeEnd() = currentDate <= dateRange.upper
+    fun notReachedEndOfRepetitions() = when (frequency) {
+        Single -> currentRepetition == 1
+        else -> isIndeterminate || currentRepetition <= repeat
+    }
+    fun mayAffectRange() = notExceededRangeEnd() && notReachedEndOfRepetitions()
+
+    while (mayAffectRange()) {
+        if (currentDate in dateRange)
+            yield(
+                RepeatingTransaction(
+                    this@repeatWhileAffecting,
+                    currentDate,
+                    currentRepetition
+                )
+            )
+
+        currentDate = startDate.shiftedBy(frequency, currentRepetition)
+        currentRepetition++
+    }
+}.toList()
+
+private fun Date.shiftedBy(frequency: Transaction.Frequency, amount: Int) = when (frequency) {
+    Single -> this
+    Daily -> this plusDays amount
+    Weekly -> this plusWeeks amount
+    Monthly -> this plusMonths amount
+    Yearly -> this plusYears amount
+}
+
+private infix fun Date.plusDays(amount: Int) = calendar.apply { add(Calendar.DAY_OF_MONTH, amount) }.time
+private infix fun Date.plusWeeks(amount: Int) = calendar.apply { add(Calendar.WEEK_OF_YEAR, amount) }.time
+private infix fun Date.plusMonths(amount: Int) = calendar.apply { add(Calendar.MONTH, amount) }.time
+private infix fun Date.plusYears(amount: Int) = calendar.apply { add(Calendar.YEAR, amount) }.time
+private val Date.calendar get() = Calendar.getInstance().also { it.time = this }
 
 data class RepeatingTransaction(val transaction: Transaction, val atDate: Date, private val number: Int) : TransactionData by transaction {
     val repeatability by lazy {
